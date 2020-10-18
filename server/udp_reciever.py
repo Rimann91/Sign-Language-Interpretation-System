d9f5227fb293e308d3cd0ecd9fdcbbb0a20aacb4
Riley <Rimannnhs@gmail.com>
2020-10-18 13:48:00 -0500

    Scoped into a class, added jpg over tcp method

--- server/udp_reciever.py
+++ server/udp_reciever.py
@@ -9,71 +9,104 @@ HEADER_SIZE = 40
 FRAME_TIMEOUT = 0.1
 UDP_IP="localhost"
 UDP_PORT = 8888
+MEDIAPIPE_PORT = 4433
+MEDIAPIP_IP = "localhost"
+FRAME_SEND_TIME = 1
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
+class server():
+    def __init__(self):
 
-print("server started....")
+        try:
+            self.sock_mp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock_mp.connect((MEDIAPIP_IP, MEDIAPIPE_PORT))
+            #sock.settimeout(1)
+        except socket.error as e:
+            print(f"Failed to create TCP socket for mediapipe connection...  {e}")
 
-frame_heap=[]
-timeout = 0
-frame_size = 0
-new_frame=True
 
-while True:
-    buffer = [b'' for x in range(FRAME_DIVISIONS)]
-    #wait_buffer = []
-    current_frame = 0
-    current_frames = []
-    new_frame = True
+    def udp_recieve(self):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.bind((UDP_IP, UDP_PORT))
 
-    while True:
-        data, addr = sock.recvfrom(46120)
+        print("server started....")
 
-        if new_frame or (time.time()-timeout>FRAME_TIMEOUT):
-            current_frame = int(data[10:20].decode('utf-8'))
-            if len(frame_heap) > 0:
-                buffer = heappop(frame_heap)[1:]
-                timeout = time.time()
-                current_frames.remove(current_frames)
-            new_frame = False
+        frame_heap=[]
+        timeout = 0
+        frame_size = 0
+        new_frame=True
+        frame_timer = time.time()
 
-        frame_size = int(data[:10].decode('utf-8'))
-        frame_sequence = int(data[10:20].decode('utf-8'))
-        slice_size = int(data[20:30].decode('utf-8'))
-        slice_sequence = int(data[30:40].decode('utf-8'))
-        #new_slice = False
-
-        if frame_sequence == current_frame:
-            #if data[HEADER_SIZE:] not in buffer:
-            buffer[slice_sequence] = data[HEADER_SIZE:]
-        
-        else:
-            #wait_buffer.insert(slice_sequence, data[HEADER_SIZE:])
-            #frame_buffer[frame_sequence].insert(slice_sequence, data[HEADER_SIZE:])
-
-            # if frame not already started
-            if frame_sequence not in current_frames:
-                d = [data[HEADER_SIZE:]]
-                d.insert(0, frame_sequence)
-                heappush(frame_heap, d)
-                current_frames.append(frame_sequence)
-            else:
-                frame_heap[current_frames.index(frame_sequence)].insert(slice_sequence, [data[HEADER_SIZE:]])
-        
-        if len(b''.join(buffer)) == (frame_size):
-            buffer = b''.join(buffer[:frame_size])
-            frame = numpy.fromstring(buffer, dtype=numpy.uint8)
-            buffer = numpy.fromstring(buffer, dtype=numpy.uint8)
-            frame = frame.reshape(480,640,3)
-
-            cv2.imshow("SERVER", frame)
-
+        while True:
+            buffer = [b'' for x in range(FRAME_DIVISIONS)]
+            #wait_buffer = []
+            current_frame = 0
+            current_frames = []
             new_frame = True
 
-            break
+            while True:
+                data, addr = sock.recvfrom(46120)
+
+                if new_frame or (time.time()-timeout>FRAME_TIMEOUT):
+                    current_frame = int(data[10:20].decode('utf-8'))
+                    if len(frame_heap) > 0:
+                        buffer = heappop(frame_heap)[1:]
+                        timeout = time.time()
+                        current_frames.remove(current_frames)
+                    new_frame = False
+
+                frame_size = int(data[:10].decode('utf-8'))
+                frame_sequence = int(data[10:20].decode('utf-8'))
+                slice_size = int(data[20:30].decode('utf-8'))
+                slice_sequence = int(data[30:40].decode('utf-8'))
+                #new_slice = False
+
+                if frame_sequence == current_frame:
+                    #if data[HEADER_SIZE:] not in buffer:
+                    buffer[slice_sequence] = data[HEADER_SIZE:]
+                
+                else:
+                    #wait_buffer.insert(slice_sequence, data[HEADER_SIZE:])
+                    #frame_buffer[frame_sequence].insert(slice_sequence, data[HEADER_SIZE:])
+
+                    # if frame not already started
+                    if frame_sequence not in current_frames:
+                        d = [data[HEADER_SIZE:]]
+                        d.insert(0, frame_sequence)
+                        heappush(frame_heap, d)
+                        current_frames.append(frame_sequence)
+                    else:
+                        frame_heap[current_frames.index(frame_sequence)].insert(slice_sequence, [data[HEADER_SIZE:]])
+                
+                if len(b''.join(buffer)) == (frame_size):
+                    buffer = b''.join(buffer[:frame_size])
+                    frame = numpy.fromstring(buffer, dtype=numpy.uint8)
+                    buffer = numpy.fromstring(buffer, dtype=numpy.uint8)
+                    frame = frame.reshape(480,640,3)
+
+                    cv2.imshow("SERVER", frame)
+
+                    if (time.time() - frame_timer) > FRAME_SEND_TIME:
+                        self.tcp_sendtomp(frame)
+                        frame_timer = time.time()
+
+                    new_frame = True
+
+                    break
 
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
 
+    def tcp_sendtomp(self, frame):
+        
+        _,frame_jpg = cv2.imencode('.jpg', frame)
+        cv2.imshow("mediapipe image",frame)
+        try:
+            self.sock_mp.sendall(frame_jpg)
+        except socket.error as e:
+            print("Failed sending jpg to mediapipe... {e}")
+        
+server = server()
+server.udp_recieve()
+
+main()
