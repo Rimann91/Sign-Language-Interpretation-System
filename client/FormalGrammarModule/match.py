1fd8e82c01d264fa7a6ae6b987d2b89f1effa5f7
Riley <Rimannnhs@gmail.com>
2020-11-10 14:53:11 -0600

    formal grammar module added

---
+++ client/FormalGrammarModule/match.py
@@ -0,0 +1,73 @@
+import sys
+from pattern.en import parse, pprint
+from autocorrect import Speller
+from pandas import DataFrame
+
+
+class ParseCommand():
+    def __init__(self):
+        pass
+
+    def get_action(self, command_df):
+        #TODO catch incorrectly identified prepositions
+        dfAction = command_df.query("TAG=='VB' or CHUNK=='I-VP'")[['WORD']]
+        action = []
+
+        for label, content in dfAction.WORD.items():
+            action.append(content)
+
+        action = ' '.join(action)
+        return action
+
+    def get_objects(self, command_df):
+        #TODO catch adjectives of the object as well to use for identification
+        dfobject = command_df.query("TAG=='NN' or TAG=='NNS'")
+
+        objects = []
+
+        for label, content in dfobject.WORD.items():
+            objects.append(content)
+
+        objects = ' '.join(objects)
+        return objects
+
+    def parse_command(self, strInput):
+        lsParsed = parse(strInput, relations=True, lemmeta=True).split()
+        df = DataFrame(lsParsed[0], columns=["WORD", "TAG", "CHUNK", "ROLE", "PNP"])
+        action = self.get_action(df)
+        objects = self.get_objects(df)
+        command = f"{action} {objects}"
+        return command
+
+    def get_command(self, strInput):
+
+        #TODO convert numberical number representation to alphabetical number representation
+            
+        # Autocorrect Spelling    
+        check = Speller(lang='en')
+        corrected = check(strInput)
+
+        # Parse the command into action-object structure
+        command = self.parse_command(corrected)
+        return command
+
+
+if len(sys.argv) > 1:
+    parser = ParseCommand()
+    if sys.argv[1] == "file":
+        with open("test_inputs.txt", 'r') as f:
+            test = f.readlines()
+            for c in test:
+                command = parser.get_command(c)
+                print(f"{c} -> {command}")
+
+    if sys.argv[1] == "key":
+        while True:
+            test = input("**> ")
+            if test != 'q':
+                command = parser.get_command(test)
+                print(f"{test} -> {command}")
+            else: break
+
+    else: print("argument error: enter a valid argument for match.py")
+else: print("Please provide arguments")
