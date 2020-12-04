111435cfab7bae42f59226e9c91fe954633f2b3c
ZLang08 <35502816+ZLang08@users.noreply.github.com>
2020-12-03 21:25:57 -0600

    Adding Apache License

--- server/Gesture-Recognition-Module/command_output_demo.py
+++ server/Gesture-Recognition-Module/command_output_demo.py
@@ -1,6 +1,23 @@
+#    Copyright 2020 Braden Bagby, Robert Stonner, Riley Hughes, David Gray, Zachary Langford
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#        http://www.apache.org/licenses/LICENSE-2.0
+
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+# Created by Zach Langford and Riley Hughes
+
+
 import requests
 
-# Interation between IFTTT and the software to trigger the smart home event
+# Interaction between IFTTT and the software to trigger the smart home event
 def control_lights(state):
     requests.get("https://maker.ifttt.com/trigger/"+state+"/with/key/lQAvLfKbKh9JHMq9zkdkQkcYoPLK9NY0LS8pqrgHEZz")
 
--- server/mediapipe-basic-handtracker-custom-files/hand-gesture-recognition-calculator.cc
+++ server/mediapipe-basic-handtracker-custom-files/hand-gesture-recognition-calculator.cc
@@ -1,5 +1,21 @@
+// Copyright 2019 The MediaPipe Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// An example of sending OpenCV webcam frames into a MediaPipe graph.
+// This example requires a linux computer and a GPU with EGL support drivers.
 
-////Mdified by Braden Bagby
+////Modified by Braden Bagby
 ////Modifications marked starting with ------------Modification BB------------------
 ///and ending with ------------Modification END BB------------------
 
